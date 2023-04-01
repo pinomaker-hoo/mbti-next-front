91c171b1a0310dbcf82623c15591459c29f00f29
pinomaker-hoo <inhoo23@naver.com>
2023-04-01 15:42:58 +0900

    feat : Main Page View

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,31 @@
+import { Grid, Typography, Button } from '@mui/material'
+import Link from 'next/link'
+
 export default function Home() {
-  return <div>Hello world</div>
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12} sx={{ ml: 14, mt: 5 }}>
+        <Typography variant="h1">너 내</Typography>
+        <Typography variant="h1">MBTI</Typography>
+        <Typography variant="h1"> 알아??</Typography>
+      </Grid>
+      <Grid item xs={12} sx={{ textAlign: 'center', mt: 45 }}>
+        <Link href="/user">
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{
+              p: 2,
+              width: '80%',
+              backgroundColor: '#ffe812',
+              color: 'black',
+              boxShadow: 'none',
+            }}
+          >
+            카카오톡으로 시작하기
+          </Button>
+        </Link>
+      </Grid>
+    </Grid>
+  )
 }
