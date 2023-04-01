56687c0646c36221f5125cea913f9e80a1c49360
pinomaker-hoo <inhoo23@naver.com>
2023-04-01 12:40:40 +0900

    feat : Mui Theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,13 @@
 import type { AppProps } from 'next/app'
 
+// ** Theme Imports
+import { ThemeProvider } from '@mui/material/styles'
+import theme from 'theme/index'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
