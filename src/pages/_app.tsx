5bbdd36726f815b7e1dea835f3993581f3277345
pinomaker-hoo <inhoo23@naver.com>
2023-04-02 11:58:02 +0900

    feat : Mui Css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 
 // ** Theme Imports
 import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import theme from 'theme/index'
 import 'styles/globals.css'
 
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
     </Provider>
