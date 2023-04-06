7406a560803019f1da68330d169b269c11f14edd
pinomaker-hoo <inhoo23@naver.com>
2023-04-06 21:35:34 +0900

    feat : Set Next proxy  config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://mbti.pinodev.shop:8080/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
