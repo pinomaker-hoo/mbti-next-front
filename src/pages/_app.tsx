df24d9c7a9afe1482cd79433522c67aee5f1dccd
pinomaker-hoo <inhoo23@naver.com>
2023-04-08 13:17:40 +0900

    fix : global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 
 // ** Theme Imports
 import { ThemeProvider } from '@mui/material/styles'
+import { GlobalStyles } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline'
 import theme from 'theme/index'
 import 'styles/globals.css'
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
+        <GlobalStyles styles={{ backgroundColor: 'blue' }} />
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
