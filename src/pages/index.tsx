da83b44956f92dddc621b07ac07411bfd1116350
pinomaker-hoo <inhoo23@naver.com>
2023-04-08 13:24:07 +0900

    feat : Main Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,25 +8,38 @@ export default function Home() {
   return (
     <Grid container spacing={6}>
       <Grid item xs={12} sx={{ ml: 14, mt: 5 }}>
-        <Typography variant="h1">너 내</Typography>
-        <Typography variant="h1">MBTI</Typography>
-        <Typography variant="h1"> 알아??</Typography>
+        <Typography variant="h1" sx={{ color: 'primary.main' }}>
+          너 내
+        </Typography>
+        <Typography variant="h1" sx={{ color: 'primary.main' }}>
+          MBTI
+        </Typography>
+        <Typography variant="h1" sx={{ color: 'primary.main' }}>
+          알아??
+        </Typography>
       </Grid>
       <Grid item xs={12} sx={{ textAlign: 'center', mt: 45 }}>
-        <Link href="/register">
+        <Link href="/login" style={{ textDecoration: 'none' }}>
           <Button
             variant="contained"
             fullWidth
             sx={{
               p: 2,
               width: '80%',
-              backgroundColor: '#ffe812',
-              color: 'black',
+              backgroundColor: 'primary.main',
+              color: 'white',
               boxShadow: 'none',
             }}
           >
-            카카오톡으로 시작하기
+            로그인 하고 시작하기!
           </Button>
+        </Link>
+      </Grid>
+      <Grid item xs={12} sx={{ textAlign: 'right', mr: 5, mt: -5 }}>
+        <Link href="/register" style={{ textDecoration: 'none' }}>
+          <Typography variant="body2" sx={{ color: 'primary.main' }}>
+            계정이 아직 없다면??
+          </Typography>
         </Link>
       </Grid>
     </Grid>
