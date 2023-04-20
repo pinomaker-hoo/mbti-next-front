d1186458262ed9e86dab18363bbcecc4d405e8bc
pinomaker-hoo <inhoo987654321@gmail.com>
2023-04-20 17:32:12 +0900

    feat : 배포 준비

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,8 +9,19 @@ export default function Document() {
           name="description"
           content="너 내 MBTI 알아??, 상대방이 생각하는 당신의 MBTI!"
         />
+        <meta
+          name="keywords"
+          content="SWYG, MBTI, PINOMAKER, Toy, 심리테스트, 엠비티아이"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <script
+          defer
+          src="https://cdn.swygbro.com/public/widget/swyg-widget.js"
+        ></script>
+
+        <link rel="apple-touch-icon" href="/mbti.png" />
+        <link rel="shortcut icon" href="/mbti.png" />
+        <link rel="icon" href="/mbti.png" />
       </Head>
       <body>
         <Main />
